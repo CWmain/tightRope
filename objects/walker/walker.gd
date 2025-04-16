dde8a8709a698eb6ae840c3c60ffdd16758e6ede
CWmain <cameronwhite2478@gmail.com>
2025-04-16 14:57:40 +1000

    Added fallen signal to walker

--- objects/walker/walker.gd
+++ objects/walker/walker.gd
@@ -4,8 +4,15 @@ extends Node2D
 
 @onready var rigid_body_2d: RigidBody2D = $RigidBody2D
 
+signal fallen
+
 func _physics_process(delta: float) -> void:
-	rigid_body_2d.apply_torque_impulse(50)
+	if rigid_body_2d.rotation > PI/2 || rigid_body_2d.rotation < -PI/2:
+		fallen.emit()
+		printerr("LOST")
+	
+	
+	rigid_body_2d.apply_torque(1000)
 	if Input.is_action_pressed("Left"):
 		rigid_body_2d.apply_torque_impulse(-torqueForce)
 		
