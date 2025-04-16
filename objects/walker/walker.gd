64ba9acb5d6d3decf29f3f4d85174458708a70c5
CWmain <cameronwhite2478@gmail.com>
2025-04-17 02:21:16 +1000

    Added new animations to the start of the game, and a toggable variable to walker to allow them to start playing before the animation completes. This is done to allow the GO to remain for a second before vanashing

--- objects/walker/walker.gd
+++ objects/walker/walker.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @export var torqueForce: float = 10000
+@export var isActive: bool = false
 
 @onready var rigid_body_2d: RigidBody2D = $RigidBody2D
 
@@ -8,13 +9,19 @@ var bonusForce: float = 0
 
 signal fallen
 
+
+
 func _physics_process(delta: float) -> void:
+	if !isActive:
+		rigid_body_2d.angular_velocity = 0
+		return
+	
 	if rigid_body_2d.rotation > PI/2 || rigid_body_2d.rotation < -PI/2:
 		fallen.emit()
 		printerr("LOST")
 	
 	
-	rigid_body_2d.apply_torque(2000)
+	#rigid_body_2d.apply_torque(2000)
 	if Input.is_action_pressed("Left"):
 		rigid_body_2d.apply_torque_impulse(-(torqueForce+bonusForce))
 		bonusForce += 100
