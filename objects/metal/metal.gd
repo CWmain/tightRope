14b5e79b575f213784e0b58f3309f09ceb37c9ff
CWmain <cameronwhite2478@gmail.com>
2025-04-18 15:35:38 +1000

    Added a random pitch shift to metal sound to make it sound better

--- objects/metal/metal.gd
+++ objects/metal/metal.gd
@@ -8,6 +8,8 @@ func _process(delta: float) -> void:
 		queue_free()
 
 func _on_body_entered(body: Node) -> void:
+	var pitchOffest: float = (randf()*0.5)-0.25
+	clank_sound.pitch_scale = 1+pitchOffest
 	clank_sound.play()
 	collision_layer = 0
 	collision_mask = 0
