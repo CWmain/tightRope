7fe0352322624bc091d99fc2b8266e77a7ebebc6
CWmain <cameronwhite2478@gmail.com>
2025-04-18 15:31:52 +1000

    Moved images into correct folders, and delteed some unneed ones

--- objects/metal/metal.gd
+++ objects/metal/metal.gd
@@ -3,10 +3,11 @@ extends RigidBody2D
 @onready var clank_sound: AudioStreamPlayer = $ClankSound
 
 
-func _on_body_entered(body: Node) -> void:
+func _process(delta: float) -> void:
+	if position.y > 200 and !clank_sound.playing:
+		queue_free()
 
-	print("here")
-	
+func _on_body_entered(body: Node) -> void:
 	clank_sound.play()
 	collision_layer = 0
 	collision_mask = 0
