1183d109ff67733dea2ae0fd1d4a3cd1a9c56e16
CWmain <cameronwhite2478@gmail.com>
2025-04-18 18:31:58 +1000

    Added a export for the location of the warning symbol. Right now each indivdual spawner is responsible for their warning

--- objects/rockSpawner/rock_spawner.gd
+++ objects/rockSpawner/rock_spawner.gd
@@ -1,10 +1,14 @@
 extends Node2D
 
 @export var rock: PackedScene
+@export var globalWarningLocation: Vector2
 @export var spawnSpeed: Vector2
+
+@onready var warning: AnimatedSprite2D = $Warning
 
 func _ready() -> void:
 	assert(rock != null)
+	warning.position = to_local(globalWarningLocation)
 
 func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("SpawnRock"):
