6e2e02dabc2b23585ba3e452dc0398b0b793088e
CWmain <cameronwhite2478@gmail.com>
2025-04-18 19:32:33 +1000

    Added the warning symbol to rockSpawner. It now runs in editor so I can see where the warning will show up in game without launching. Futhermore the warning symbol gives a 1 second warning before the object is launched (can no longer spam but once patterns are implemented than the timer can start that, which should be good

--- objects/rockSpawner/rock_spawner.gd
+++ objects/rockSpawner/rock_spawner.gd
@@ -1,18 +1,33 @@
+@tool
 extends Node2D
 
 @export var rock: PackedScene
-@export var globalWarningLocation: Vector2
+@export var globalWarningLocation: Vector2:
+	set(value):
+		globalWarningLocation = value
+		if warning != null:
+			warning.position = to_local(globalWarningLocation)
 @export var spawnSpeed: Vector2
 
 @onready var warning: AnimatedSprite2D = $Warning
+@onready var attack_timer: Timer = $attackTimer
 
 func _ready() -> void:
 	assert(rock != null)
-	warning.position = to_local(globalWarningLocation)
+	if !Engine.is_editor_hint():
+		warning.position = to_local(globalWarningLocation)
+		warning.hide()
 
 func _physics_process(delta: float) -> void:
+	if Engine.is_editor_hint():
+		return
 	if Input.is_action_just_pressed("SpawnRock"):
-		var newRock: RigidBody2D = rock.instantiate()
-		newRock.linear_velocity = spawnSpeed
-		add_child(newRock)
+		warning.show()
+		attack_timer.start()
 		
+
+func _on_attack_timer_timeout() -> void:
+	var newRock: RigidBody2D = rock.instantiate()
+	newRock.linear_velocity = spawnSpeed
+	add_child(newRock)
+	warning.hide()
