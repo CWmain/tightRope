d33515551f82b8d74c9f2abba6fda1fd3e26750a
CWmain <cameronwhite2478@gmail.com>
2025-04-23 19:20:58 +1000

    Added quit animation, adjusted exsisting animation to use rotation instead of position as changing the position of controls breaks stuff

--- UI/endGameMenu/end_game_menu.gd
+++ UI/endGameMenu/end_game_menu.gd
@@ -5,8 +5,6 @@ extends Control
 var mainMenu: PackedScene = load("res://scenes/mainMenu/main_menu.tscn")
 @onready var endGameMenuScore: PointsDisplay = $Buttons/pointsDisplay
 
-
-
 ## Called by game manager to trigger the game end animation
 func gameEndAnimation() -> void:
 	animation_player.play("gameEnd")
@@ -24,10 +22,11 @@ func _on_sound_settings_close_menu() -> void:
 	animation_player.play("gameEndToSound", -1, -1, true)
 
 func _on_quit_pressed() -> void:
-	get_tree().change_scene_to_packed(mainMenu)
-
+	animation_player.play("quit")
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	match anim_name:
 		"play":
 			get_tree().reload_current_scene()
+		"quit":
+			get_tree().change_scene_to_packed(mainMenu)
